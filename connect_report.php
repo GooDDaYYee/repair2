8b71438214deac425060210e2aaa4b9295d6ee4e
GooDDaYYee <GooDDaYYee@gmail.com>
2020-10-29 14:34:45 +0700

    Web

--- connect_report.php
+++ connect_report.php
@@ -3,7 +3,6 @@ $serverName = "localhost";
 $userName = "root";
 $userPassword = ""; //รหัสผ่านฐานข้อมูล
 $dbName = "repair"; //ชื่อฐานข้อมูล
-$tbNeme = "detail";
 
 $conn = mysqli_connect($serverName,$userName,$userPassword,$dbName,$tbNeme);
 // Check connection
